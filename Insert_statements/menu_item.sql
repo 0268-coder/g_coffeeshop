56be7bd8449b4df1ca1b067a5d5062649b3000d4
Jzguyr <guyhd9119@gmail.com>
2025-11-18 21:52:07 +0700

    wtf menu_item mai kob pen puan kan por

--- Insert_statements/menu_item.sql
+++ Insert_statements/menu_item.sql
@@ -1,3 +1,11 @@
 INSERT INTO `menu_item` (`menu_item_id`, `menu_item_name`, `menu_category`, `menu_price`, `menu_is_seasonal`, `menu_available_from`, `menu_available_to`) VALUES
+(1, 'Espresso', 'coffee', '192.00', 0, NULL, NULL),
+(2, 'Americano', 'coffee', '224.00', 0, NULL, NULL),
+(3, 'Latte', 'coffee', '256.00', 0, NULL, NULL),
+(4, 'Cappuccino', 'coffee', '256.00', 0, NULL, NULL),
+(5, 'Flat White', 'coffee', '288.00', 0, NULL, NULL),
+(6, 'Mocha', 'coffee', '304.00', 0, NULL, NULL),
+(7, 'Hot Chocolate', 'non-coffee', '288.00', 0, NULL, NULL),
+(8, 'Matcha Latte', 'non-coffee', '320.00', 0, NULL, NULL),
 (9, 'Chai Latte', 'non-coffee', '192.00', 0, NULL, NULL),
 (10, 'Double Espresso', 'coffee', '224.00', 0, NULL, NULL);
